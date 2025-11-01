07b666f61f17d49a158ac359d4309871c90edcd5
Victorydev1 <aniduemeekesea@gmail.com>
2025-11-01 09:56:53 +0100

    social probs

--- components/ui/Social.tsx
+++ components/ui/Social.tsx
@@ -2,23 +2,30 @@ import React from 'react'
 import Link from 'next/link'
 import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
 
-const socials = [
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedinIn />, path: "" },
-    { icon: <FaYoutube />, path: "" },
-    { icon: <FaTwitter />, path: "" },
-];
-
-const Social = ({containerStyles, iconStyles}) => {
-  return (
-    <div className={containerStyles}>
-        {socials.map((item, index)=> {
-            return <Link key={index} href={item.path} className={iconStyles}>
-                {item.icon}
-            </Link>
-        })}
-    </div>
-  )
+// Define prop types for your component
+interface SocialProps {
+  containerStyles?: string;
+  iconStyles?: string;
 }
 
-export default Social
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+const Social: React.FC<SocialProps> = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => (
+        <Link key={index} href={item.path} className={iconStyles}>
+          {item.icon}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Social;
+ 
